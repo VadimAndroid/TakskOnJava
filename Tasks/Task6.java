5027658b66ad314a57dbe57d4e78f41f158b2e53
Krypton <va6714776@gmail.com>
2023-04-08 16:52:44 +0300

    New files updated

--- Tasks/Task6.java
+++ Tasks/Task6.java
@@ -1,2 +1,24 @@
 public class Task6 {
-}
+    public static void main(String[] args) {
+      int result1 = Task6.makeNegative(1);
+      System.out.println(result1);  // prints -1
+      
+      int result2 = Task6.makeNegative(-5);
+      System.out.println(result2);  // prints -5
+      
+      int result3 = Task6.makeNegative(0);
+      System.out.println(result3);  // prints 0
+    }
+    
+    
+        public static int makeNegative(final int x) {
+          if (x > 0) {
+            return -x;
+          }
+          return x;
+        }
+      
+  }
+  
+
+  
