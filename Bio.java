cdd94af456c9bbadb635ae3806e60cf66ceba08c
vadimAndoid <va6714776@gmail.com>
2023-04-08 15:08:52 +0300

    Upload new files

--- Bio.java
+++ Bio.java
@@ -1,2 +1,14 @@
 public class Bio {
+    public String dnaToRna(String dna) {
+        StringBuilder rna = new StringBuilder();
+        for (int i = 0; i < dna.length(); i++) {
+            char nucleotide = dna.charAt(i);
+            if (nucleotide == 'T') {
+                rna.append('U');
+            } else {
+                rna.append(nucleotide);
+            }
+        }
+        return rna.toString();
+    }
 }
